b2976da2bccc72a9162b7aefdd4162054a9d02f1
Tony dela cruz <delacruztony.adc@gmail.com>
2020-10-20 02:39:45 +0800

    moved firebase config properties to env

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAoszdXRDYSSu4XxbEeCU4jvd5QFzU0urQ",
-  authDomain: "myflix-cd750.firebaseapp.com",
-  databaseURL: "https://myflix-cd750.firebaseio.com",
-  projectId: "myflix-cd750",
-  storageBucket: "myflix-cd750.appspot.com",
-  messagingSenderId: "632415789315",
-  appId: "1:632415789315:web:7db4029a05c1db5e8cc936",
-  measurementId: "G-E3JM2H6TRS",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 export const firebaseApp = firebase.initializeApp(firebaseConfig);
